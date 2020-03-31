0d38ac4b137f3bf7ade40d63ef2b29cad71fa5bb
student <student@mbp-stud-10.local>
2020-03-30 21:23:27 -0500

    First Commit

---
+++ MAD157FontsVanA/FavoritesList.swift
@@ -0,0 +1,48 @@
+//
+//  FavoritesList.swift
+//  MAD157FontsVanA
+//
+//  Created by student on 3/29/20.
+//  Copyright © 2020 vanderson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FavoritesList {
+    static let sharedFavoritesList = FavoritesList()
+    private(set) var favorites:[String]
+    
+    init() {
+        let defaults = UserDefaults.standard
+        let storedFavorites = defaults.object(forKey: "favorites") as? [String]
+        favorites = storedFavorites != nil ? storedFavorites! : []
+    }
+    func addFavorite(fontName: String) {
+        if !favorites.contains(fontName) {
+            favorites.append(fontName)
+            saveFavorites()
+        }
+    }
+    
+    func removeFavorite(fontName: String) {
+        if let index = favorites.index(of: fontName) {
+            favorites.remove(at: index)
+            saveFavorites()
+        }
+    }
+    
+    private func saveFavorites() {
+        let defaults = UserDefaults.standard
+        defaults.set(favorites, forKey: "favorites")
+        defaults.synchronize()
+    }
+    
+    func moveItem(fromIndex from: Int, toIndex to: Int) {
+        let item = favorites[from]
+        favorites.remove(at: from)
+        favorites.insert(item, at: to)
+        saveFavorites()
+    }
+    
+}
---
+++ MAD157FontsVanA/FontInfoViewController.swift
@@ -0,0 +1,56 @@
+//
+//  FontInfoViewController.swift
+//  MAD157FontsVanA
+//
+//  Created by student on 3/30/20.
+//  Copyright © 2020 vanderson. All rights reserved.
+//
+
+import UIKit
+
+class FontInfoViewController: UIViewController {
+
+    var font: UIFont!
+    var favorite: Bool = false
+    @IBOutlet weak var fontSampleLabel: UILabel!
+    @IBOutlet weak var fontSizeSlider: UISlider!
+    @IBOutlet weak var fontSizeLabel: UILabel!
+    @IBOutlet weak var favoriteSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        fontSampleLabel.font = font
+        fontSampleLabel.text = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVv" + "WwXxYyZz 0123456789"
+        fontSizeSlider.value = Float(font.pointSize)
+        fontSizeLabel.text = "\(Int(font.pointSize))"
+        favoriteSwitch.isOn = favorite
+    }
+    
+    @IBAction func slideFontSize(slider: UISlider) {
+        let newSize = roundf(slider.value)
+        fontSampleLabel.font = font.withSize(CGFloat(newSize))
+        fontSizeLabel.text = "\(Int(newSize))"
+    }
+    
+    @IBAction func toggleFavorite(sender: UISwitch) {
+        let favoritesList = FavoritesList.sharedFavoritesList
+        if sender.isOn {
+            favoritesList.addFavorite(fontName: font.fontName)
+        } else {
+            favoritesList.removeFavorite(fontName: font.fontName)
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
